2b94ffbbac00e1dea8e8928fc41274db8991f1d2
studyzhb <445798850@qq.com>
2016-12-05 16:09:39 +0800

    editor3

--- wd/js/goodsdetail.js
+++ wd/js/goodsdetail.js
@@ -107,3 +107,66 @@ var ImageWrapper={
 
 
 
+      // 拖拽
+      $("#box div").mousedown(function(e){
+        var that = this;
+        var disX=e.offsetX, disY=e.offsetY;
+        var $clone = $("<div>");
+        $clone.addClass("draging").css("left",$(this).position().left).css("top",$(this).position().top).html($(this).html());
+        $(this).addClass("moving").html("");
+        $clone.appendTo($(this).parent());
+        $("#box").on("mousemove",function(e){
+          //悬浮层跟随鼠标移动
+          $clone.css("left",e.clientX-$(this).offset().left-disX).css("top",e.clientY-$(this).offset().top-disY);
+        });
+        $clone.mouseup(function(){
+          //取消拖拽事件
+          $("#box").off("mousemove");
+          //判断最小距离，进行交换
+          var swapIndex = $(that).index();
+          var minDistance = 1000;
+          $("#box div:not(:last)").each(function(){
+            var dis = Math.sqrt(Math.pow($clone.position().left-$(this).position().left, 2) +
+              Math.pow($clone.position().top-$(this).position().top, 2));
+            
+            if(dis<minDistance){
+              minDistance = dis;
+              swapIndex = $(this).index();
+            }
+          });
+          
+          if(swapIndex == $(that).index() ){
+            //恢复原位
+            $clone.animate($(that).position(),200,function(){
+              $(that).removeClass("moving").html($clone.html());
+              $clone.remove();
+            })
+          } else {
+            //交换位置元素
+            var $swapEle = $("#box div").eq(swapIndex);
+            //生成第二个需要运动的复制元素
+            var $clone2 = $("<div>");
+            //将复制元素的位置设定在交换元素上
+            $clone2.addClass("draging").css("left", $swapEle.position().left ).css("top", $swapEle.position().top ).html($swapEle.html());
+            //将交换元素的样式暂时改为移动状态
+            $swapEle.addClass("moving").html("");
+            //将复制元素2加入九宫格
+            $("#box").append($clone2);
+            //交换位置, 复制元素1移动至交换元素处
+            $clone.animate($swapEle.position(),400, function(){
+              $swapEle.removeClass("moving").html( $clone.html() ); 
+              $clone.remove(); //消失，同时交换元素恢复状态
+            });
+            //复制元素2移动至初始元素处
+            $clone2.animate( $(that).position(),400, function(){
+              $(that).removeClass("moving").html( $clone2.html() );
+              $clone2.remove();//消失，同时初始元素恢复状态
+              
+            });
+          }
+        });
+      });
+
+
+
+
