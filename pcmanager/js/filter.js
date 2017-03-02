2623877d5cbfc64275cfa9107ab29c0c5a96e6c6
zhb <445798850@qq.com>
2017-03-02 09:59:46 +0800

    移动端赚钱花钱页面

---
+++ pcmanager/js/filter.js
@@ -0,0 +1,160 @@
+
+var layer;
+var $;
+var laytpl;
+var user=cookieUtil.getCookie('username');
+var cookAuthor=cookieUtil.getCookie('authorlist');
+var localAuthor=
+$(function(){
+	
+	/*$('.layui-tab-title').on('click',function(){
+		if($(this).has('a')){
+			location.href=$(this).find('a').attr('href');
+		}
+	})*/
+
+	$('.quit').on('click',function(){
+	
+	config.ajax('get',config.ajaxAddress.quit,function(data){
+		if(data.code==200){
+            layer.msg('退出成功');
+            cookieUtil.removeCookie('username');
+            cookieUtil.removeCookie('authorlist');
+            cookieUtil.removeCookie('token');
+            setTimeout(function(){
+                open('login.html','_self');
+            },500);
+            
+        }else{
+            layer.msg('网络错误，请稍后重试');
+            setTimeout(function(){
+                open('login.html','_self');
+            },500);
+        }
+	})
+	});
+
+	// config.ajax('get',config.ajaxAddress.validateAuthor,function(data){
+	// 	console.log(data);
+	// 	if(data.code==200){
+           
+            
+ //        }else{
+ //            layer.msg('请检查权限');
+ //            setTimeout(function(){
+ //                open('login.html','_self');
+ //            },500)
+ //        }
+	// });
+
+// 对菜单列表进行归类
+
+var netPath=location.href;
+var reg=/(\/[a-zA-Z0-9-_]+)/g;
+var getArr=netPath.match(reg);
+var switchVal=getArr[getArr.length-1].substring(1);
+switch(switchVal){
+	case 'add-brand':
+		netPath+='/goodsbrand.html';
+		break;
+	case 'editor-brand':
+		netPath+='/goodsbrand.html';
+		break;
+	case 'add-sort':
+		netPath+='/goodsSort.html';
+		break;
+	case 'editor-sort':
+		netPath+='/goodsSort.html';
+		break;
+	case 'editor-goodsInfo':
+		netPath+='/editorgoodsInfo.html';
+		break;
+	case 'add-admin-menu':
+		netPath+='/admin-menulist.html';
+		break;
+	case 'editor-author':
+		netPath+='/authorlist.html';
+		break;
+	case 'add-author':
+		netPath+='/authorlist.html';
+		break;
+	case 'add-shop-origin':
+		netPath+='/shop.html';
+		break;
+	case 'shopprice-his':
+		netPath+='/shop-price.html';
+		break;
+	case　'add-psort':
+		netPath+='/pSort.html';
+		break;
+	case 'editor-psort':
+		netPath+='/pSort.html';
+		break;
+		
+}
+// if(!cookAuthor){
+	config.ajax('get',config.ajaxAddress.getAuthorlist,function(data){
+		console.log(data.code,typeof data);
+		if(data.code=='401'||!data.code){
+			open('login.html','_self');
+			return;
+		}
+		if(!!user){
+			$('.username').text(cookieUtil.getCookie('username'));
+		}else{
+			open('login.html','_self');
+		}
+		cookieUtil.setExpiresDate('authorlist',JSON.stringify(data));
+		updateList(data.data);
+	});
+// }else{
+// 	if(!!user){
+// 			$('.username').text(cookieUtil.getCookie('username'));
+// 		}else{
+// 			open('login.html','_self');
+// 		}
+// 	updateList(JSON.parse(cookAuthor));
+// }
+
+function updateList(data)
+{
+	layui.use(['laypage','layer','laytpl'],function(){
+				$=layui.jquery;
+		    	laytpl = layui.laytpl;
+		    	layer = layui.layer;
+		    	var tempHtml=slider.innerHTML;
+		    	//获取权限列表	
+
+		    	$.each(data,function(index,item){
+					// console.log(item.children);
+					item.thisselc=netPath;
+					item.shai=item.thisselc;
+					
+					laytpl(tempHtml).render(item,function(html){
+					// console.log(html);
+					
+					$('#sliderPage').append(html);
+
+					})
+				})
+				layui.use('element',function(){
+					var element = layui.element();
+					element.init();
+				});
+		 });
+}
+$('.show-content').on('click','.select-tit',function(){
+
+        if($(this).next().css('display')=='none'){
+            $(this).nextAll().css('display','block');
+        }else{
+            $(this).nextAll().css('display','none');
+        } 
+    });
+
+    $('.show-content').on('click','li',function(){
+        $(this).parents('.select-items').css('display','none').next().css('display','none');
+        $(this).parents('.select-items').prev('.select-tit').find('span').text($(this).text());  
+    })
+
+});
---
+++ pcmanager/js/routers.js
@@ -0,0 +1,75 @@
+$(function(){
+	/*var laytpl;
+	layui.use('laytpl',function(){
+		laytpl = layui.laytpl;
+		config.ajax('get',config.ajaxAddress.shopList,function(data){
+			console.log(data);
+			var tempHtml=menulistContent.innerHTML;
+			//console.log(tempHtml);
+			
+				$('#menulist-wraper').html('');
+				$.each(data,function(index,item){
+					console.log(item);
+					
+					laytpl(tempHtml).render(item,function(html){
+						$('#menulist-wraper').append(html);
+					});
+				});
+		})
+	
+	});
+
+	$('#menulist-wraper').on('click','.editorShopInfo',function(){
+		open('edit-shop.html?id='+$(this).data('id'),'_self');
+	});
+
+	$('#all-author-list').on('click','.editor-role',function(){
+		
+	});*/
+	var w=$('.shop-content').width();
+	var h=$('.shop-content').height();
+	
+	$('.layui-nav-child').on('click','dd a',function() {
+		var url=$(this).data('url');
+		showIframe(url,w,h);
+	});
+
+
+	// showIframe('me.html',w,h);
+	// showIframe('detail.html',w,h);
+
+
+	function showIframe(url,w,h){
+	    //添加iframe
+	    var if_w = w; 
+	    var if_h = h; 
+	    //allowTransparency='true' 设置背景透明
+		$('.shop-content').html('');
+	    $("<iframe width='" + if_w + "' height='" + if_h + "' id='YuFrame1' name='YuFrame1' style='z-index:4;'  frameborder='no' marginheight='0' marginwidth='0' allowTransparency='true'></iframe>").appendTo($('.shop-content'));    
+	    var st=document.documentElement.scrollTop|| document.body.scrollTop;//滚动条距顶部的距离
+	    var sl=document.documentElement.scrollLeft|| document.body.scrollLeft;//滚动条距左边的距离
+	    var ch=document.documentElement.clientHeight;//屏幕的高度
+	    var cw=document.documentElement.clientWidth;//屏幕的宽度
+	    var objH=$("#YuFrame1").height();//浮动对象的高度
+	    var objW=$("#YuFrame1").width();//浮动对象的宽度
+	    var objT=Number(st)+(Number(ch)-Number(objH))/2;
+	    var objL=Number(sl)+(Number(cw)-Number(objW))/2;
+	    $("#YuFrame1").css('left','0');
+	    $("#YuFrame1").css('top','0');
+	 
+	    $("#YuFrame1").attr("src", url)
+	 
+	    //添加背景遮罩
+	    //$("<div id='YuFrame1Bg' style='background-color: Gray;display:block;z-index:3;position:absolute;left:0px;top:0px;filter:Alpha(Opacity=30);/* IE */-moz-opacity:0.4;/* Moz + FF */opacity: 0.4; '/>").prependTo('body'); 
+	    /*var bgWidth = Math.max($("body").width(),cw);
+	    var bgHeight = Math.max($("body").height(),ch);
+	    $("#YuFrame1Bg").css({width:bgWidth,height:bgHeight});
+	 */
+	    //点击背景遮罩移除iframe和背景
+	    /*$("#YuFrame1Bg").click(function() {
+	        $("#YuFrame1").remove();
+	        $("#YuFrame1Bg").remove();
+	    });*/
+	}
+
+});
