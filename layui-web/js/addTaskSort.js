56121748f3ef677f3291e4757a37774abcf372a4
zhb <445798850@qq.com>
2017-03-29 16:09:17 +0800

    手机端页面

--- layui-web/js/addTaskSort.js
+++ layui-web/js/addTaskSort.js
@@ -1,16 +1,45 @@
 $(function(){
+
+	var taskSort={
+		data:{
+			sortData:[]
+		}
+	}
+
 	config.ajax('get',config.ajaxAddress.taskSort.addSort,function(data){
 		console.log(data);
-		$('.parCom').html('<option value="0">一级机构</option>');
+		$('.parCom').html('<option value="0" selected>一级机构</option>');
+		taskSort.data.sortData=data.data;
 		$.each(data.data,function(index,item){
-			$('<option>').appendTo($('.parCom')).html(item.name).attr('value',item.id);
+			$('<option>').appendTo($('.parCom')).html(item.name).attr('value',item.id).attr('data-type',item.type);
 			// $.each(item.children,function(i,its){
 			// 	$('<option>').appendTo($('.parCom')).html(' ---- '+its.type);
 			// });	
 		});
 		
 		layui.use('form',function(){
-			
+			var form=layui.form();
+			form.on('select(sortWrapper)',function(data){
+				$('.sortTypeWrapper').html('');
+				if(data.value==0){
+					$('.sortTypeWrapper').show();
+					$('.sortTypeWrapper').append($('.conditionWrapper').html());
+					form.render();
+				}else{
+					$('.sortTypeWrapper').hide();
+					var sType;
+					$.each(taskSort.data.sortData,function(index,item){
+						if(item.id==data.value){
+							sType=item.type;
+						}
+					})
+					$('.sortTypeWrapper').append(config.formatTemplate({type:sType},$('.readTypeCon').html()));
+				}
+			})	
+
+			form.on('select(sortFilter)',function(data){
+				
+			})
 		});
 	})
 
